2d3c03d914f0618611014bc2a99dbd63cb3bc7df
Mishka <mishka.s.mukherji@gmail.com>
2015-03-11 17:17:01 -0400

    working movie generator in first test

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :movie do
-    
+    sequence(:title) { |n| "Movie #{n}" }
+    sequence(:rating) {|n| ["g","pg","pg-13","r","nc-17","unrated"].sample}
   end
-
 end
